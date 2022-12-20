5138f81f29779c59adda4d380f4ed469690189c2
Simon Sywanyk <simon.sywanyk@gmail.com>
2022-12-20 15:01:21 -0500

    added JS API

---
+++ js/api/KanbanAPI.js
@@ -0,0 +1,54 @@
+export default class KanbanAPI {
+    static getItems(columnId) {
+        const column = read().find(column => column.id == columnId)
+
+        if(!column) {
+            return []
+        }
+
+    return column.items
+    }
+
+    static insertItem(columnId, content) {
+        const data = read()
+        const column = read().find(column => column.id == columnId)
+        const item = {
+            id: Math.floor(Math.random() * 1000),
+            content
+        }
+
+        if(!column) {
+            throw new Error("Column does not exist")
+        }
+    }
+}
+
+function read() {
+    const json = localStorage.getItem("kanban-data")
+
+    if(!json) {
+        return [
+            {
+                id: 1,
+                items: [],
+            },
+            {
+                id: 2,
+                items: [],
+            },
+            {
+                id: 3,
+                items: [],
+            },
+            {
+                id: 4,
+                items: [],
+            },
+        ]
+    }
+    return JSON.parse(json)
+}
+
+function save(data) {
+    localStorage.setItem("kanban-data", JSON.stringify(data))
+}
