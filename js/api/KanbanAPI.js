726824b63bfc0b2cb927a29d0bbb21afcac237b0
Simon Sywanyk <simon.sywanyk@gmail.com>
2022-12-20 18:41:36 -0500

    updated api

--- js/api/KanbanAPI.js
+++ js/api/KanbanAPI.js
@@ -14,18 +14,69 @@ export default class KanbanAPI {
         const column = read().find(column => column.id == columnId)
         const item = {
             id: Math.floor(Math.random() * 1000),
-            content
+            content,
         }
 
         if(!column) {
             throw new Error("Column does not exist")
         }
+
+        column.items.push(item)
+        save(data)
+
+        return item
+    }
+
+    static updateItem(itemId, newProps){
+        const data = read()
+        const [item, currentColumn] (() => {
+            for (const column of data) {
+                const item = column.items.find(item => item.id == itemId)
+
+                if(item){
+                    return [item,column]
+                }
+            }
+        })
+
+        if(!item){
+            throw new Error("Item not found.")
+        }
+
+        item.content = newProps.content === undefined ? item.content : newProps.content
+
+        if(newProps.columnId !== undefined && newProps.position !== undefined) {
+            const targetColumn = data.find(column => column.id == newProps.columnId)
+
+            if(!targetColumn){
+                throw new Errror("Target column not found.")
+            }
+            //find current column & splice data
+            currentColumn.items.splice(currentColumn.items.indexOf(item),1)
+            //find target column & splice data
+            targetColumn.items.split(newProps.position,0,item)
+        }
+
+        save(data)
+    }
+
+    static deleteItem(itemID) {
+        const data = read()
+
+        for (const column of data) {
+            const item = column.items.find(item => item.id == itemId)
+
+            if(item) {
+                column.items.splice(column.items.indexOf(item),1)
+            }
+        }
+        save(data)
     }
 }
 
 function read() {
     const json = localStorage.getItem("kanban-data")
-
+//if not data, prefill with four empty items
     if(!json) {
         return [
             {
